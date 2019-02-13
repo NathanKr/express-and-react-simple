a67427351b7211307592bfcd64ed58a0ed16776a
Nathan Krasney <natankrasney@gmail.com>
2019-02-13 21:02:41 +0200

    fix a bug using proxy

--- server/client/README.md
+++ server/client/README.md
@@ -3,6 +3,6 @@ check the server readme about this.
 
 The following should be done to make this work
 <ul>
-<li>add "proxy": "http://localhost:5000/api" to the client package.json</li>
+<li>add "proxy": "http://localhost:5000" to the client package.json</li>
 <li>later the client access /api which is production ready. when /api is not found as in developemnt the proxy is used</li>
 </ul>
--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from "react";
 import "./App.css";
 
+
 class App extends Component {
-  url = "/api";
+  url = "http:/api";
+  state = {data : ''};
+
   clickHandler = () => {
     console.log("clicked");
     fetch(this.url)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({data : data.res}))
       .catch(err => console.log(err));
   };
+
   render() {
     return (
       <div className="App">
         <p>Hello Nathan Krasney</p>
         <button onClick={this.clickHandler}>Access express server</button>
+        <p>Got : {this.state.data}</p>
       </div>
     );
   }
