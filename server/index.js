a0cfdfeaf3ff02f3b5f7f52d1f10f7cf6fdd995d
Nathan Krasney Laptop <natankrasney@gmail.com>
2020-01-14 16:19:17 +0200

    use express.json

--- server/index.js
+++ server/index.js
@@ -1,13 +1,10 @@
 console.log("app is loading");
-
 const express = require("express");
-
 const app = express();
 
-app.get("/api", (req, res) => {
-  console.log("root is accessed");
-  res.send({res:"result from server"});
-});
+// used for json inside body 
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
